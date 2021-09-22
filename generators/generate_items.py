68d05bb9091572b96eb5b2e461cd929aee6d0bcf
Geegaz <geegazrd@gmail.com>
2021-09-22 17:25:24 +0200

    added item generator

--- generators/generate_items.py
+++ generators/generate_items.py
@@ -1,36 +1,44 @@
 
 from items import *
+import csv
 
-class Umbrella(CustomItem):
-
-    def __init__(self, color) -> None:
-        super().__init__(
-            base_item = "minecraft:shield",
-            cmd = 1707231 + COLORS.index(color),
-            id = f"{color}_umbrella",
-            traits=["item", "umbrella"])
-        
-        self.recipe["pattern"] = [
-            "###",
-            " / ",
-            " / "
-        ]
-        self.recipe["key"] = {
-            "#": {
-                "item": f"minecraft:{color}_carpet"
-            },
-            "/": {
-                "item": "minecraft:stick"
-            }
-        }
-
-def generate_all():
-    lang = {}
+def make_umbrellas(generator: ItemGenerator) -> None:
     for color in COLORS:
-        umbrella = Umbrella(color)
-        umbrella.generate()
-        lang[f"{NAMESPACE}.item.{umbrella.id}"] = ""
+        umbrella = CustomItem(
+            base_item="minecraft:shield",
+            id=f"{color}_umbrella",
+            cmd=1707231 + COLORS.index(color),
+            traits=["item", "umbrella"],
+            generator=generator)
+
+        # Make the data
+        umbrella.create_advancement()
+        umbrella.create_loot_table()
+        umbrella.create_function()
+        umbrella.create_recipe({
+            "type": "minecraft:crafting_shaped",
+            "pattern": [
+                "###",
+                " / ",
+                " / "
+            ],
+            "key": {
+                "#": {
+                    "item": f"minecraft:{color}_carpet"
+                },
+                "/": {
+                    "item": "minecraft:stick"
+                }
+            },
+            "result": {
+                "item": "minecraft:knowledge_book",
+                "count": 1
+            }
+        })
+
 
 #-------- Generate --------#
-generate_all()
-    
+generator = ItemGenerator("geegaz", "gghe")
+make_umbrellas(generator)
+
+generator.generate_all()
--- generators/items.py
+++ generators/items.py
@@ -1,4 +1,5 @@
 
+from os import name
 from pathlib import Path, PurePath
 import json
 
@@ -24,106 +25,158 @@ COLORS = [
     "green"
 ]
 
+
 class Type:
     RECIPE = "recipes"
     ADVANCEMENT = "advancements"
     LOOT_TABLE = "loot_tables"
     FUNCTION = "functions"
 
+
+class ItemGenerator:
+
+    dev_name: str
+    namespace: str
+
+    items: dict
+    lang: dict
+    paths: dict
+
+    def __init__(self, dev_name, namespace) -> None:
+        self.dev_name = dev_name
+        self.namespace = namespace
+
+        self.items = {}
+        self.lang = {}
+        self.paths = {
+            Type.RECIPE: "",
+            Type.ADVANCEMENT: "recipes/",
+            Type.LOOT_TABLE: "items/",
+            Type.FUNCTION: "recipes/"
+        }
+
+    def register(self, item) -> None:
+        self.items[item.id] = item
+        self.lang[item.id] = {"id": f"{NAMESPACE}.item.{item.id}"}
+
+    def generate(self, item) -> None:
+        print("Generating", item.id)
+
+        for data_type, data in item.data.items():
+            # Create path
+            path = Path(f"data/{self.namespace}/{data_type}/{self.paths[data_type]}")
+            path.mkdir(parents=True, exist_ok=True)
+            if data_type == Type.FUNCTION:
+                path = path.joinpath(f"{item.id}.mcfunction")
+                str_data = "\n".join(data)
+            else:
+                path = path.joinpath(f"{item.id}.json")
+                str_data = json.dumps(data, indent=4)
+
+            # Write file
+            path.write_text(str_data)
+
+    def generate_all(self) -> None:
+        for item in self.items.values():
+            print(item)
+            self.generate(item)
+
+
 class CustomItem:
 
+    base_item: str
     id: str
     cmd: int
+    traits: list
 
-    recipe: dict
-    advancement: dict
-    loot_table: dict
-    function: list
+    generator : ItemGenerator
 
-    PATHS = {
-        Type.RECIPE: "",
-        Type.ADVANCEMENT: "recipes/",
-        Type.LOOT_TABLE: "items/",
-        Type.FUNCTION: "recipes/"
-    }
+    data: dict
 
-    def __init__(self, base_item: str, id: str, cmd: int, traits=[]) -> None:
+    def __init__(self, generator: ItemGenerator, base_item: str, id: str, cmd: int, traits=[]) -> None:
+        self.base_item = base_item
         self.id = id
         self.cmd = cmd
-        self.recipe = {
-            "type": "minecraft:crafting_shaped",
-            "pattern": [],
-            "key": {},
-            "result": {
-                "item": "minecraft:knowledge_book",
-                "count": 1
-            }
-        }
-        self.advancement = {
-            "criteria": {
-                "requirement": {
-                    "trigger": "minecraft:recipe_unlocked",
-                    "conditions": {
-                        "recipe": f"{NAMESPACE}:{self.PATHS[Type.RECIPE]}{self.id}"
-                    }
+        self.traits = traits
+        
+        self.generator = generator
+        generator.register(self)
+        
+        self.data = {}
+
+    def create_recipe(self, recipe = None):
+        if recipe:
+            self.data[Type.RECIPE] = recipe
+        else:
+            self.data[Type.RECIPE] = {
+                "type": "minecraft:crafting_shaped",
+                "pattern": [],
+                "key": {},
+                "result": {
+                    "item": "minecraft:knowledge_book",
+                    "count": 1
                 }
-            },
-            "rewards": {
-                "function": f"{NAMESPACE}:{self.PATHS[Type.FUNCTION]}{self.id}",
-                "loot": [
-                    f"{NAMESPACE}:{self.PATHS[Type.LOOT_TABLE]}{self.id}"
-                ]
             }
-        }
-        self.loot_table = {
-            "pools": [
-                {
-                    "rolls": 1,
-                    "entries": [
-                        {
-                            "type": "minecraft:item",
-                            "name": base_item,
-                            "functions": [
-                                {
-                                    "function": "minecraft:set_name",
-                                    "name": {
-                                        "translate": f"{NAMESPACE}.item.{self.id}",
-                                        "italic": False
-                                    }
-                                },
-                                {
-                                    "function": "minecraft:set_nbt",
-                                    "tag": f"{{ctc:{{id:'{self.id}',from:'{DEVNAME}:{NAMESPACE}',traits:{traits}}},CustomModelData:{self.cmd}}}"
-                                }
-                            ]
+
+    def create_advancement(self, advancement = None):
+        if advancement:
+            self.data[Type.ADVANCEMENT] = advancement
+        else:
+            self.data[Type.ADVANCEMENT] = {
+                "criteria": {
+                    "requirement": {
+                        "trigger": "minecraft:recipe_unlocked",
+                        "conditions": {
+                            "recipe": f"{self.generator.namespace}:{self.generator.paths[Type.RECIPE]}{self.id}"
                         }
+                    }
+                },
+                "rewards": {
+                    "function": f"{self.generator.namespace}:{self.generator.paths[Type.FUNCTION]}{self.id}",
+                    "loot": [
+                        f"{self.generator.namespace}:{self.generator.paths[Type.LOOT_TABLE]}{self.id}"
                     ]
                 }
+            }
+
+    def create_loot_table(self, loot_table = None):
+        if loot_table:
+            self.data[Type.LOOT_TABLE] = loot_table
+        else:
+            self.data[Type.LOOT_TABLE] = {
+                "pools": [
+                    {
+                        "rolls": 1,
+                        "entries": [
+                            {
+                                "type": "minecraft:item",
+                                "name": self.base_item,
+                                "functions": [
+                                    {
+                                        "function": "minecraft:set_name",
+                                        "name": {
+                                            "translate": f"{self.generator.namespace}.item.{self.id}",
+                                            "italic": False
+                                        }
+                                    },
+                                    {
+                                        "function": "minecraft:set_nbt",
+                                        "tag": f"{{ctc:{{id:'{self.id}',from:'{self.generator.dev_name}:{self.generator.namespace}',traits:{self.traits}}},CustomModelData:{self.cmd}}}"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+
+    def create_function(self, function = None):
+        if function:
+            self.data[Type.FUNCTION] = function
+        else:
+            self.data[Type.FUNCTION] = [
+                "clear @s knowledge_book 1",
+                f"recipe take @s {self.generator.namespace}:{self.generator.paths[Type.RECIPE]}{self.id}",
+                f"advancement revoke @s only {self.generator.namespace}:{self.generator.paths[Type.ADVANCEMENT]}{self.id}"
             ]
-        }
-        self.function = [
-            "clear @s knowledge_book 1"
-            f"recipe take @s {NAMESPACE}:{self.PATHS[Type.RECIPE]}{self.id}",
-            f"advancement revoke @s only {NAMESPACE}:{self.PATHS[Type.ADVANCEMENT]}{self.id}"
-        ]
-
-    def generate(self):
-        print("Generating", self.id)
-
-        path: Path
-        data = {
-            Type.RECIPE: json.dumps(self.recipe,indent=4),
-            Type.ADVANCEMENT: json.dumps(self.advancement,indent=4),
-            Type.LOOT_TABLE: json.dumps(self.loot_table,indent=4),
-            Type.FUNCTION: "\n".join(self.function)
-        }
-        for key, value in self.PATHS:
-            # Create path
-            path = Path(f"data/{NAMESPACE}/{key}/{value}/{self.id}")
-            path.mkdir(parents=True, exist_ok=True)
-            if key == Type.FUNCTION:
-                path.joinpath(".mcfunction")
-            else:
-                path.joinpath(".json")
-
-            path.write_text(data[key])
+            
